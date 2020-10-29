e99678c3aab131d87f2f82dabc090aa8956d0b93
gabrielgobbi <icloud.gobbi@gmail.com>
2020-10-29 11:46:40 -0300

    firt

---
+++ app/Models/Teste.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Teste extends Model
+{
+    use HasFactory, LogsActivity;
+
+    protected $fillable = ['teste'];
+
+    protected static $logAttributes = ['name', 'text'];
+
+    protected static $logName = 'Teste';
+
+}
